f2345ad32427b9d1356e6af0ef9ddacaee01dfab
Ammon King <ammon.j.king@gmail.com>
2019-02-08 07:14:41 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'search-filter';
+  words = [
+    "Açaí",
+    "Apple",
+    "Akee",
+    "Apricot",
+    "Avocado",
+    "Banana",
+    "Bilberry",
+    "Blackberry",
+    "Blackcurrant",
+    "Black sapote",
+    "Blueberry",
+    "Boysenberry",
+    "Buddha's hand (fingered citron)",
+    "Crab apples",
+    "Currant",
+    "Cherry",
+    "Cherimoya (Custard Apple)",
+    "Chico fruit",
+    "Cloudberry",
+    "Coconut",
+    "Cranberry"
+  ];
+
+  filteredWords = this.words;
+
+  term = "";
+
+  doSearch() {
+    this.filteredWords = this.words.filter(s =>
+      s.toLowerCase().includes(this.term.toLowerCase())
+    );
+  }
+
+  resetSearch() {
+    this.term = "";
+    this.filteredWords = this.words;
+  }
 }
